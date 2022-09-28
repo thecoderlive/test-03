31ab0dc2b25fcd3a25349d8dc96b6ceee6db1c6d
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:21:55 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "61bf60e6-5de1-4948-9a21-d885745ad0dc",
+    "home_feed": [
+        {
+            "id": "4fc4ffb8-a9ba-4180-b6b2-355294458d30",
+            "artist_name": "direction",
+            "home_carousel": [
+                {
+                    "id": "da1eb345-19a1-42fd-bd87-9098eb01286f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "6f889668-c322-4431-978f-c802d189c1d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "0d7a7cd9-dc14-4aa1-905f-39fed709c024",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "0c45cb62-4766-40a7-894b-250a717b6b5b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "1da1aeb1-732d-419a-949b-04297128c240",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                }
+            ],
+            "art_caption": "by",
+            "art_description": "Guy economy health station. Yes stand religious."
+        },
+        {
+            "id": "47d0a349-406c-484a-a29d-cfdb9bccff6b",
+            "artist_name": "skin",
+            "home_carousel": [
+                {
+                    "id": "96a78c34-183c-4a64-a449-d66519fd1b07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "955aca2b-5b9c-45c7-b228-9e93cbd3e0c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "d111aaf6-9885-45cb-8496-b778e2e7cd47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png"
+                },
+                {
+                    "id": "475393e3-3bfa-4875-910e-610d4f1fa860",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "553db4e0-a0c4-4b95-ad9c-35acea92ecb0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                }
+            ],
+            "art_caption": "spend",
+            "art_description": "Occur name special win admit political. Plant practice side order well recent."
+        },
+        {
+            "id": "19d97b62-4daa-4e9e-a0fa-9ac67887e486",
+            "artist_name": "country",
+            "home_carousel": [
+                {
+                    "id": "830a31b2-a0cd-4cc4-b38f-77cd509bae63",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "66148c1f-4f39-4f90-9a5b-444553809cb2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/300.png"
+                },
+                {
+                    "id": "471939a7-c722-4d48-ad67-246b92a9be83",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "0edc443a-2a77-4c6e-98e8-2f6554b6fcde",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "085c6b49-ce43-43da-9303-b52ac160284f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/320.png"
+                }
+            ],
+            "art_caption": "store",
+            "art_description": "Sister way wear American perform. Forward role less lay war. Visit political the car traditional."
+        },
+        {
+            "id": "414ad44c-341b-4421-8ec6-5edbf7d12800",
+            "artist_name": "argue",
+            "home_carousel": [
+                {
+                    "id": "ce13f439-2075-4214-9f4d-09837ec2106a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "7bed0f16-5b69-4328-a9f1-4910640f81d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                },
+                {
+                    "id": "0197e02b-9773-4703-b4e8-7e48f66b1d12",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/178.png"
+                },
+                {
+                    "id": "2f0113f6-7cb4-4995-a713-e4f127c63114",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png"
+                },
+                {
+                    "id": "79d6156e-ca72-4760-ac13-bddbff50829d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                }
+            ],
+            "art_caption": "seven",
+            "art_description": "Movie house score morning positive. Money much whose student."
+        },
+        {
+            "id": "8929091a-7cec-41cb-85e8-1e2222955524",
+            "artist_name": "onto",
+            "home_carousel": [
+                {
+                    "id": "1634e1a1-8ec0-4362-b18a-499fdc4eb4c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/203.png"
+                },
+                {
+                    "id": "a4e867c7-fdfd-4e25-8676-d7b74c35b71e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "d78b6cc2-63bc-4fab-bb4d-2026860f60c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "9366c7ca-70d9-48c8-afdc-47a19698fc6c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                },
+                {
+                    "id": "f0324edc-74a9-430d-a0ae-f93b1d3eb0a5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                }
+            ],
+            "art_caption": "listen",
+            "art_description": "Least should quite concern beautiful prove key. Woman common material voice traditional use the. Gun scene share lawyer water health."
+        }
+    ]
+}
